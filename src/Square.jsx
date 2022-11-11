231195001414c209e73ced6163b1d9bd00607b40
Tiara Temple <tmtemple7@gmail.com>
2022-11-11 16:48:19 -0600

    tic-tac-toe fully functional; proj 1 complete

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 const Square = (props) => {
-  console.log(props.squares, props.player)
+  console.log('props', props)
+
+  const handleClick = () => {
+    if (!props.squareValue) {
+      if (props.player) {
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      } else {
+        props.squares.splice(props.index, 1, "O");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      }
+    }
+  };
+ 
    return (
-    <div className="square">
+    <div className="square" onClick={handleClick}>
+      {props.squareValue === 'O' ? 'O' : props.squareValue}
     </div>
   );
 };
